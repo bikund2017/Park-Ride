1c3a0b8ca730d05b50987b151bd79ee990b6dd0f
bikundkumar <bikund2017@outlook.com>
2025-10-30 01:17:34 +0530

    remove

--- api/favorites.js
+++ api/favorites.js
@@ -48,7 +48,6 @@ export default async function handler(req, res) {
   if (allowedOrigins.includes(origin)) {
     res.setHeader('Access-Control-Allow-Origin', origin);
   }
-  
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
